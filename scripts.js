0295f85f566f91a4971515c04ed3ec08b912106d
whdgk159 <whdgk159@gmail.com>
2023-01-04 23:03:25 +0900

    Extracting_cooridinates_and_colors_from_raw_pixelData

--- scripts.js
+++ scripts.js
@@ -7,10 +7,13 @@ window.addEventListener('load', function(){
     canvas.height = this.window.innerHeight;
 
     class Particle {
-        constructor(effect){
+        constructor(effect, x, y, color){
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
+            this.color = color;
             this.size = 10;
             this.vx = Math.random() * 2 -1;
             this.vy = Math.random() * 2 -1;
@@ -34,6 +37,7 @@ window.addEventListener('load', function(){
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
+            this.gap = 5;
         }
         init(context){
             /*for (let i = 0; i <100; i++){
@@ -42,6 +46,20 @@ window.addEventListener('load', function(){
             context.drawImage(this.image, this.x, this.y);
             const pixels = context.getImageData(0,0, this.width, this.height);
             console.log(pixels);
+            for (let  y= 0; y < this.height; y += this.gap){
+                for (let x = 0; x < this.width; x += this.gap){
+                    const index = (y * this.width + x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const alpha = pixels[index + 3];
+                    const color = 'rgb('+ red +', '+ green +', '+ blue +' )';
+
+                    if(alpha > 0){
+                        this.particleArray.push(new Particle(this, x, y, color));
+                    }
+                }
+            }
         }
         draw(context){
             this.particleArray.forEach(Particle => Particle.draw(context));
